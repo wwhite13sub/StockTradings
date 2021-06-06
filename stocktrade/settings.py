d0d06df70ad3a3aeb24b8e7f4f09ca04871c147c
Wanda White <wanda.white32@yahoo.com>
2021-06-05 22:24:32 -0500

    updating settings

--- stocktrade/settings.py
+++ stocktrade/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3p#2l_4re#a+u&frpr_w(=bh&04$9_z2b)4(-very87+ljo21-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carstocks.herokuapp.com']
 
 
 # Application definition
