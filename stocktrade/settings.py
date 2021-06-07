7bfa048882cdc5297db229eba3ee5c0cd866ec59
Wanda White <wanda.white32@yahoo.com>
2021-06-07 01:53:12 -0500

    updating settings

--- stocktrade/settings.py
+++ stocktrade/settings.py
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'stocktrade.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         # BASE_DIR / 'db.sqlite3',
+#     }
+# }
 import dj_database_url
 DATABASE_URL = config("DATABASE_URL")
 DATABASES = {}
